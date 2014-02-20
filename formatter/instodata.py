dea32f4468aeae381c8700905acf76f85703587c
Scott <walentys@gmail.com>
2014-02-20 02:50:48 -0500

    added dl to formatter, also used it to form giant table at beginning of bank 03

--- formatter/instodata.py
+++ formatter/instodata.py
@@ -20,11 +20,15 @@ def cum_all_over_me(filename, data_format):
 
     # grab starting address
     addr = int(addrs[0], 16)
-    for i in xrange(0, len(bytes), 4):
-        b = bytes[i:i+4]
+
+    step = 4
+    if data_format == 'dl':
+        step = 6
+    for i in xrange(0, len(bytes), step):
+        b = bytes[i:i+step]
         line = '{0}{1}{2:>13}.{3} {4}'.format(bank, format(addr, '04X'), ' ', data_format, shit_in_my_ass(data_format, b))
         print line
-        addr += 4
+        addr += step
 
 # builds up data string
 def shit_in_my_ass(data_format, bytes):
@@ -33,11 +37,13 @@ def shit_in_my_ass(data_format, bytes):
         chunks = ['${0}'.format(b) for b in bytes]
     elif data_format == 'dw':
         chunks = ['${1}{0}'.format(bytes[i], bytes[i + 1]) for i in xrange(0, len(bytes), 2)]
+    elif data_format == 'dl':
+        chunks = ['${2}{1}{0}'.format(bytes[i], bytes[i + 1], bytes[i + 2]) for i in xrange(0, len(bytes), 3)]
     return ', '.join(chunks)
 
 
 data_format = 'db'
-data_formats = ['db', 'dw']
+data_formats = ['db', 'dw', 'dl']
 if len(sys.argv) >= 3:
     if sys.argv[2] in data_formats:
         data_format = sys.argv[2]
