e2c0b08ff8b3160a411bc965851eb8fb18670b94
Scott <walentys@gmail.com>
2014-02-21 00:26:53 -0500

    ins. to data module handles almost anything, pretty cool!!!

--- formatter/instodata.py
+++ formatter/instodata.py
@@ -1,4 +1,6 @@
 import sys
+import re
+import itertools
 
 def cum_all_over_me(filename, data_format):
     bytes = []
@@ -6,39 +8,49 @@ def cum_all_over_me(filename, data_format):
     bank = '$00/'
     with open(filename, 'r') as f:     
         for line in f:
-            l = line.split()
-            addrs.append(l[0][4:8])
-            bank = l[0][0:4]
-            # remove address
-            del l[0]
-
-            for s in l:
-                # end of bytes
-                if len(s) > 2:
-                    break
-                bytes.append(s)
+            r = re.compile(r'(\$\w\w/)(\S+) ((?:\w\w(?:[ ]|\b))+)')
+            m = r.match(line)
+            if m:
+                bank = m.group(1)
+                addrs.append(m.group(2))
+                bytes.extend(m.group(3).strip().split())
 
     # grab starting address
     addr = int(addrs[0], 16)
 
-    step = 4
-    if data_format == 'dl':
-        step = 6
-    for i in xrange(0, len(bytes), step):
-        b = bytes[i:i+step]
-        line = '{0}{1}{2:>13}.{3} {4}'.format(bank, format(addr, '04X'), ' ', data_format, shit_in_my_ass(data_format, b))
-        print line
-        addr += step
+    size_step = [1, 4]
+    if data_format == 'db':
+        size_step = [1, 4]
+    elif data_format == 'dw':
+        size_step = [2, 4]
+    elif data_format == 'dl':
+        size_step = [3, 6]
+
+    for i in xrange(0, len(bytes), size_step[1]):
+        b = bytes[i:i + size_step[1]]
+        if len(b) < size_step[1] and len(b) % size_step[0] != 0:
+            if len(b) - size_step[0] > 0:
+                # there is poop left in the butthole
+                poop = b[0:len(b) - (len(b) % size_step[0])]
+                one_line(bank, addr, data_format, size_step[0], poop)
+                addr += len(poop)
+                del b[0:len(b) - (len(b) % size_step[0])]
+
+            # change size to 1 for remainder!!!!
+            size_step[0] = 1
+            data_format = 'db'
+
+        one_line(bank, addr, data_format, size_step[0], b)
+        addr += size_step[1]
+
+# prints a full line of bytes
+def one_line(bank, addr, data_format, size, bytes):
+    line = '{0}{1}{2:>13}.{3} {4}'.format(bank, format(addr, '04X'), ' ', data_format, shit_in_my_ass(size, bytes))
+    print line
 
 # builds up data string
-def shit_in_my_ass(data_format, bytes):
-    chunks = []
-    if data_format == 'db':
-        chunks = ['${0}'.format(b) for b in bytes]
-    elif data_format == 'dw':
-        chunks = ['${1}{0}'.format(bytes[i], bytes[i + 1]) for i in xrange(0, len(bytes), 2)]
-    elif data_format == 'dl':
-        chunks = ['${2}{1}{0}'.format(bytes[i], bytes[i + 1], bytes[i + 2]) for i in xrange(0, len(bytes), 3)]
+def shit_in_my_ass(size, bytes):
+    chunks = ['$' + ''.join([bytes[i + j] for j in reversed(xrange(size))]) for i in xrange(0, len(bytes), size)]
     return ', '.join(chunks)
 
 
