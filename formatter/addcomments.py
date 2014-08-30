0f5ecac78ce3e443ec63550b03d9d2d84966c714
Scott <walentys@gmail.com>
2014-08-30 01:20:08 -0400

    comment lines for bank 08

---
+++ formatter/addcomments.py
@@ -0,0 +1,14 @@
+import sys
+
+def add_comment_lines(filename):
+    with open(filename, 'r') as file:
+        for line in file:
+            l = line[0:len(line) - 1]
+            comment_line = 42
+            if ';' not in l and len(l) > 0 and len(l) < comment_line:
+                spaces = ' ' * (comment_line - len(l))
+                l = l + spaces + ';'
+
+            sys.stdout.write(l + '\n')
+
+add_comment_lines(sys.argv[1])
