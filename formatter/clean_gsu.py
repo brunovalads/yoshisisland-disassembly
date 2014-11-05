a264114a08a367c1380d2db2ff72b75b5567931d
Scott <walentys@gmail.com>
2014-11-04 20:50:43 -0500

    cleaner gsu

--- formatter/clean_gsu.py
+++ formatter/clean_gsu.py
@@ -14,24 +14,51 @@ def clean_gsu(filename):
 
                 for b in gbytes:
                     if b[0] == '1':
-                        print r_instr('to', b[1])
+                        print output_instr(bank, addr, r_instr('to', b[1]))
                     elif b[0] == '2':
-                        print r_instr('with', b[1])
+                        print output_instr(bank, addr, r_instr('with', b[1]))
                     elif b[0] == 'B':
-                        print r_instr('from', b[1])
+                        print output_instr(bank, addr, r_instr('from', b[1]))
                     elif b == '3D':
-                        print 'alt1'
+                        print output_instr(bank, addr, 'alt1')
                     elif b == '3E':
-                        print 'alt2'
+                        print output_instr(bank, addr, 'alt2')
                     elif b == '3F':
-                        print 'alt3'
+                        print output_instr(bank, addr, 'alt3')
                     else:
                         # if no alt/with/to/from, stop parsing
                         break
 
+                # branching
+                the_rest = re.sub(
+                    r'(bra|bge|blt|bne|beq|bpl|bmi|bcc|bcs|bvc|bvs)\s+(\w+)',
+                    '\\1 CODE_{0}\\2'.format(bank),
+                    the_rest)
+
+                # labeling
+                the_rest = output_instr(bank, addr, the_rest)
+
                 print the_rest
             else:
                 print line.strip()
+
+def output_instr(bank, addr, instr):
+    return label_instr(bank, addr, comment_instr(instr))
+
+def comment_instr(instr):
+    r_comm = re.compile(r'(.*)(\s+)(;)(.*)')
+    m_comm = r_comm.match(instr)
+    if m_comm:
+        com_len = 19 - len(m_comm.group(1))
+        com_spaces = ' ' * com_len
+        instr = m_comm.expand(r'\1{0}\3\4').format(com_spaces)
+    else:
+        com_len = 19 - len(instr)
+        instr += ' ' * com_len + ';'
+    return instr
+
+def label_instr(bank, addr, instr):
+    return 'CODE_{0}{1}:         {2}'.format(bank, addr, instr)
 
 def r_instr(instr, nib):
     """takes a nibble & instruction, spits out proper GSU register formatting"""
