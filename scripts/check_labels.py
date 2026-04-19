06b73df79ae90b50d3e76bd64f69ccbbdab92b13
Bruno Valadão Cunha <brunovcunha@hotmail.com>
2026-04-19 20:52:33 -0300

    This script now works on Python 3

--- scripts/check_labels.py
+++ scripts/check_labels.py
@@ -15,6 +15,6 @@ def report_bad_labels(filename):
         for line in f:
             m = r.match(line)
             if m and m.group(1) not in labels:
-                print line[0:len(line) - 1]
+                print(line[0:len(line) - 1])
 
 report_bad_labels(sys.argv[1])
